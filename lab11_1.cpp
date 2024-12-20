52008e348a3190bb269677e1242596bf984af09b
LimPrattana <vivo.twy0270@gmail.com>
2024-12-20 15:45:23 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,29 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main(){
+cout << "Press Enter 3 times to reveal your future.";
+for(int i=0; i<3;i++){
+cin.get();
+}
+
+srand(time(0));
+string g;
+int x = rand()%9;
+if(x==0) g = "A";
+else if(x==1) g = "B+";
+else if(x==2) g = "B";
+else if(x==3) g = "C+";
+else if(x==4) g = "C";
+else if(x==5) g = "D+";
+else if(x==6) g = "D";
+else if(x==7) g = "F";
+else g = "W";
+
+
+cout << "You will get "<< g <<" in this 261102.\n";
+
+}
